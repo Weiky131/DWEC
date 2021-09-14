a2f9979bf06159665a36a2c7b897797ae306ae64
Weiky131 <90708959+Weiky131@users.noreply.github.com>
2021-09-14 17:20:28 +0200

    Add files via upload

---
+++ Prova/prova.js
@@ -0,0 +1,19 @@
+function alertHM(){
+
+    alert('Hola mon!');
+}
+
+function consoleHM(){
+
+    console.log('Hola mon!');
+}
+
+function innerHM(){
+
+    document.getElementById('p').innerHTML = ('Hola mon!')
+}
+
+function docwriteHM(){
+
+    document.write('Hola mon!')
+}
